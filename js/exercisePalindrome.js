838071b3f0310351a76106b48a919919909e9d17
valentinacamacho <valentinacamacho0206@gmail.com>
2023-09-06 02:07:23 -0500

    it works when you click the button but the alert that it is not a polyndrome does not appear.

--- js/exercisePalindrome.js
+++ js/exercisePalindrome.js
@@ -2,11 +2,23 @@ const input = document.querySelector(".inpusts input");
 const btnValidation = document.querySelector(".inpusts button");
 let stringEntrada;
 
-btnValidation.addEventListener("click", () =>{
+btnValidation.addEventListener("click", () => {
+
+   let reverseInput = stringEntrada.split("").reverse().join("");
+   for (let i = 0; i <= reverseInput.length; i++) {
+    if (stringEntrada[i]!=reverseInput[reverseInput.length-i- 1]) {
+    return alert("No es una palabra palindrome");
+  }
+  return alert("Sí es una palabra palindrome");
+}
 
 });
 
 input.addEventListener("keyup", () => {
-    //validacion de letras numero y espacios.
-    stringEntrada = input.value.replace(/[A-Za-z0-9]+/ig, "");
+  //validacion de letras numero y espacios.
+   stringEntrada = input.value.toLowerCase().replace(/[A-Za-z0-9]+/ig, "");
+  if (stringEntrada) {
+    return btnValidation.classList.add("active");
+  }
+  btnValidation.classList.remove("active");
 });
