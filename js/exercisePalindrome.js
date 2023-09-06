33554cc48f956394e1dff96075eaed5bd90da3b5
valentinacamacho <valentinacamacho0206@gmail.com>
2023-09-06 08:39:45 -0500

    The logic only works for palindrome.

--- js/exercisePalindrome.js
+++ js/exercisePalindrome.js
@@ -3,20 +3,17 @@ const btnValidation = document.querySelector(".inpusts button");
 let stringEntrada;
 
 btnValidation.addEventListener("click", () => {
-
-   let reverseInput = stringEntrada.split("").reverse().join("");
-   for (let i = 0; i <= reverseInput.length; i++) {
-    if (stringEntrada[i]!=reverseInput[reverseInput.length-i- 1]) {
-    return alert("No es una palabra palindrome");
-  }
-  return alert("Sí es una palabra palindrome");
-}
-
+  let reverseInput = stringEntrada.split("").reverse().join();
+    if (reverseInput != stringEntrada) {
+      return alert("No es Palindrome")
+    }
+    alert("Es palindrome")
+ 
 });
 
 input.addEventListener("keyup", () => {
   //validacion de letras numero y espacios.
-   stringEntrada = input.value.toLowerCase().replace(/[A-Za-z0-9]+/ig, "");
+  stringEntrada = input.value.toLowerCase().replace(/[A-Za-z0-9]+/ig, "");
   if (stringEntrada) {
     return btnValidation.classList.add("active");
   }
